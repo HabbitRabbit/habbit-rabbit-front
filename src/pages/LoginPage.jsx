f30d4303ae420e3536dce9cf4dd0e0ac90c7b223
Julia Solias <juliasohu@gmail.com>
2025-03-06 15:06:38 +0100

    navigate to dashboard when logged in

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -35,7 +35,7 @@ function LoginPage(props) {
         // to the server's JWT validation endpoint. 
         authenticateUser(); 
       
-        navigate('/');                                  
+        navigate('/dashboard');                                  
       })
       .catch((error) => {
         const errorDescription = error.response.data.message;
