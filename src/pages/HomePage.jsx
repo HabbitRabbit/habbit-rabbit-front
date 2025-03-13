5999e8a799129e35d9eafd138d6c5f733c969d3d
Julia Solias <juliasohu@gmail.com>
2025-03-13 17:23:16 +0100

    path to dashboard on homepage if already logged in

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,16 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import axios from "axios";
 import logo from "../assets/Habbit-logo-alone.png"
 import { Link } from "react-router-dom";
 import Loader from "../components/Loader";
+import { AuthContext } from "../context/auth.context";
 
 
 
 function HomePage() {
+
+const { isLoggedIn } = useContext(AuthContext);
+
     return (
       <div className="flex flex-col items-center justify-center min-h-screen">
         <Link to="/login">
@@ -14,12 +18,22 @@ function HomePage() {
        </Link>
       <h1 className="text-5xl font-bold text-purple-900 mb-7 drop-shadow-md">Welcome to Habbit Rabbit</h1>
       <Loader />
-      <Link
-        to="/login"
-        className="bg-gray-200 mt-7 text-purple-800 py- px-6 rounded-full shadow-lg hover:bg-gray-100 transition duration-300 ease-in-out transform hover:scale-105"
-      >
-        Login
-      </Link>
+      {isLoggedIn ? (
+        <Link
+          to="/dashboard"
+          className="bg-gray-200 mt-7 text-purple-800 py-3 px-6 rounded-full shadow-lg hover:bg-gray-100 transition duration-300 ease-in-out transform hover:scale-105"
+        >
+          See your goals
+        </Link>
+      ) : (
+        <Link
+          to="/login"
+          className="bg-gray-200 mt-7 text-purple-800 py-3 px-6 rounded-full shadow-lg hover:bg-gray-100 transition duration-300 ease-in-out transform hover:scale-105"
+        >
+          Login
+        </Link>
+      )}
+
       <Link to="/about" className="mt-5 text-purple-600 hover:text-purple-800 transition duration-300 italic">
         Get to know us
       </Link>
