1916945a59a06eca5d22a3a1ff9cbae15df7d417
sllorens-cuenca <sllorens.cuenca@gmail.com>
2025-03-13 14:19:21 +0100

    add line

--- src/components/WeeklyView.jsx
+++ src/components/WeeklyView.jsx
@@ -182,6 +182,7 @@ const WeeklyView = ({ habits, fetchHabits, goals, fetchGoals }) => {
               <Box sx={{ width: '80%', maxWidth: '500px' }}>
                   <LinearProgress
                     variant="determinate"
+                    // Value => (achivedCount (1) / required Checks (30) ) * 100
                     value={50}
                     sx={{
                       height: '12px', // Custom height
